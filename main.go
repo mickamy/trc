f49a20e8eea8b7a2a3645136df70c6b007e73176
Tetsuro Mikami <tetsuro@mickamy.com>
2026-03-02 12:14:15 +0900

    feat: implement CLI entrypoint with subcommand routing and flag parsing

--- main.go
+++ main.go
@@ -1,11 +1,91 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"flag"
 	"fmt"
+	"os"
+	"os/signal"
 )
 
 var version = "dev"
 
 func main() {
-	fmt.Println("trc", version)
+	os.Exit(run())
+}
+
+func run() int {
+	var (
+		flags       globalFlags
+		showVersion bool
+	)
+
+	fs := flag.NewFlagSet("trc", flag.ContinueOnError)
+	fs.SetOutput(os.Stderr)
+	fs.Usage = printUsage
+	fs.StringVar(&flags.network, "network", "", "")
+	fs.StringVar(&flags.network, "n", "", "")
+	fs.StringVar(&flags.filter, "filter", "", "")
+	fs.BoolVar(&showVersion, "version", false, "")
+	fs.BoolVar(&showVersion, "v", false, "")
+
+	if err := fs.Parse(os.Args[1:]); err != nil {
+		if errors.Is(err, flag.ErrHelp) {
+			return 0
+		}
+		return 1
+	}
+
+	if showVersion {
+		fmt.Println("trc", version)
+		return 0
+	}
+
+	rest := fs.Args()
+	if len(rest) == 0 {
+		printUsage()
+		return 0
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	var err error
+	switch rest[0] {
+	case "watch":
+		err = handleWatch(ctx, flags, rest[1:])
+	case "tree":
+		err = handleTree(ctx, flags, rest[1:])
+	case "map":
+		err = handleMap(ctx, flags, rest[1:])
+	default:
+		fmt.Fprintf(os.Stderr, "trc: unknown command %q\n", rest[0])
+		printUsage()
+		return 1
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "trc: %v\n", err)
+		return 1
+	}
+	return 0
+}
+
+func printUsage() {
+	fmt.Fprintf(os.Stderr, `trc - Docker network HTTP/gRPC traffic tracer
+
+Usage:
+  trc [flags] <command> [args...]
+
+Commands:
+  watch    Stream live traffic on a Docker network
+  tree     Display a request chain as a tree (by trace-id)
+  map      Show service dependency summary
+
+Flags:
+  -n, --network    Docker network to capture (auto-detected if omitted)
+  --filter         Filter output by service name
+  --version, -v    Print version
+  -h, --help       Show this help
+`)
 }
