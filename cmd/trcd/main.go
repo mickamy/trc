ac9f6bf684ce3fd06bf8ea2bccadf529f80da652
Tetsuro Mikami <tetsuro@mickamy.com>
2026-03-02 15:15:48 +0900

    feat: add version flag to trcd

--- cmd/trcd/main.go
+++ cmd/trcd/main.go
@@ -11,6 +11,8 @@ import (
 	"github.com/mickamy/trc/internal/capture"
 )
 
+var version = "dev"
+
 func main() {
 	os.Exit(run())
 }
@@ -18,17 +20,26 @@ func main() {
 func run() int {
 	cfg := capture.DefaultConfig()
 
+	var showVersion bool
+
 	fs := flag.NewFlagSet("trcd", flag.ContinueOnError)
 	fs.SetOutput(os.Stderr)
 	fs.StringVar(&cfg.Interface, "interface", cfg.Interface, "network interface to capture")
 	fs.StringVar(&cfg.Interface, "i", cfg.Interface, "network interface to capture (short)")
 	fs.StringVar(&cfg.BPFFilter, "bpf", cfg.BPFFilter, "BPF filter expression")
+	fs.BoolVar(&showVersion, "version", false, "")
+	fs.BoolVar(&showVersion, "v", false, "")
 
 	if err := fs.Parse(os.Args[1:]); err != nil {
 		if errors.Is(err, flag.ErrHelp) {
 			return 0
 		}
 		return 1
+	}
+
+	if showVersion {
+		fmt.Println("trcd", version)
+		return 0
 	}
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
