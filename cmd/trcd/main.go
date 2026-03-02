ec521d42991ba4ad0df6b5f5749c12db0298ec60
Tetsuro Mikami <tetsuro@mickamy.com>
2026-03-02 12:36:23 +0900

    feat: implement trcd daemon with live packet capture

--- cmd/trcd/main.go
+++ cmd/trcd/main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
 	"os"
+	"os/signal"
+
+	"github.com/mickamy/trc/internal/capture"
 )
 
 func main() {
-	fmt.Fprintln(os.Stderr, "trcd: not implemented")
-	os.Exit(1)
+	os.Exit(run())
+}
+
+func run() int {
+	cfg := capture.DefaultConfig()
+
+	fs := flag.NewFlagSet("trcd", flag.ContinueOnError)
+	fs.SetOutput(os.Stderr)
+	fs.StringVar(&cfg.Interface, "interface", cfg.Interface, "network interface to capture")
+	fs.StringVar(&cfg.Interface, "i", cfg.Interface, "network interface to capture (short)")
+	fs.StringVar(&cfg.BPFFilter, "bpf", cfg.BPFFilter, "BPF filter expression")
+
+	if err := fs.Parse(os.Args[1:]); err != nil {
+		return 1
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	if err := capture.Run(ctx, cfg, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "trcd: %v\n", err)
+		return 1
+	}
+	return 0
 }
